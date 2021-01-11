6f6f473a938fc4984791bd1e0cb3c4bfbe4061fe
VincentFTS <platforms@ftsoftware.fr>
2021-01-11 16:54:22 +0100

    une

--- Table Structure Recognition/Functions/line_detection.py
+++ Table Structure Recognition/Functions/line_detection.py
@@ -9,7 +9,7 @@ def line_detection(image):
     bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 1)
     bw = cv2.bitwise_not(bw)
     ## To visualize image after thresholding ##
-    cv2_imshow("bw",bw)
+    cv2_imshow(bw)
     cv2.waitKey(0)
     ###########################################
     horizontal = bw.copy()
