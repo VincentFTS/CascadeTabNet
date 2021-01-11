6366c544096e0aebc8c39cee0a5356bacb6664bb
VincentFTS <platforms@ftsoftware.fr>
2021-01-11 18:03:19 +0100

    uieuie

--- Table Structure Recognition/Functions/line_detection.py
+++ Table Structure Recognition/Functions/line_detection.py
@@ -10,7 +10,7 @@ def line_detection(image):
     bw = cv2.bitwise_not(bw)
     ## To visualize image after thresholding ##
     cv2_imshow(bw)
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
     ###########################################
     horizontal = bw.copy()
     vertical = bw.copy()
